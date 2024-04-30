3f746139e24f862c12160a220716626b4760f79c
Alok Chowdhury <alokchy04@yahoo.com>
2024-04-30 10:26:02 +1000

    added msw mock server

---
+++ test/mock/mock-responses.ts
@@ -0,0 +1,504 @@
+export const VALID_RESPONSE = {
+  summary: {
+    query: "297 queen street",
+    queryType: "NON_NEAR",
+    queryTime: 40,
+    numResults: 10,
+    offset: 0,
+    totalResults: 100,
+    fuzzyLevel: 1,
+    queryIntent: [],
+  },
+  results: [
+    {
+      type: "Point Address",
+      id: "RRWJkuHSuDItEQhbRZxb3g",
+      score: 6.3785104752,
+      address: {
+        streetNumber: "297",
+        streetName: "Queen Street",
+        municipalitySubdivision: "Concord West",
+        municipality: "Sydney",
+        countrySecondarySubdivision: "Sydney",
+        countrySubdivision: "New South Wales",
+        countrySubdivisionName: "New South Wales",
+        countrySubdivisionCode: "NSW",
+        postalCode: "2138",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress:
+          "297 Queen Street, Concord West, New South Wales, 2138",
+        localName: "Concord West",
+      },
+      position: {
+        lat: -33.844132,
+        lon: 151.086392,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -33.84323,
+          lon: 151.08531,
+        },
+        btmRightPoint: {
+          lat: -33.84503,
+          lon: 151.08747,
+        },
+      },
+      entryPoints: [
+        {
+          type: "main",
+          position: {
+            lat: -33.84415,
+            lon: 151.08608,
+          },
+        },
+      ],
+    },
+    {
+      type: "Point Address",
+      id: "yO5pmQM6gdwXt4IdJyQyXQ",
+      score: 6.3785104752,
+      address: {
+        streetNumber: "297",
+        streetName: "Queen Street",
+        municipalitySubdivision: "Campbelltown",
+        municipality: "Sydney",
+        countrySecondarySubdivision: "Sydney",
+        countrySubdivision: "New South Wales",
+        countrySubdivisionName: "New South Wales",
+        countrySubdivisionCode: "NSW",
+        postalCode: "2560",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress:
+          "297 Queen Street, Campbelltown, New South Wales, 2560",
+        localName: "Campbelltown",
+      },
+      position: {
+        lat: -34.069745,
+        lon: 150.811095,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -34.06885,
+          lon: 150.81001,
+        },
+        btmRightPoint: {
+          lat: -34.07064,
+          lon: 150.81218,
+        },
+      },
+      entryPoints: [
+        {
+          type: "main",
+          position: {
+            lat: -34.06993,
+            lon: 150.81134,
+          },
+        },
+      ],
+    },
+    {
+      type: "Point Address",
+      id: "lOuGpLdQTxVYb_u4bnTgEQ",
+      score: 6.3563818932,
+      address: {
+        streetNumber: "287-297",
+        streetName: "Queen Street",
+        municipalitySubdivision: "Melbourne CBD",
+        municipality: "Melbourne",
+        countrySecondarySubdivision: "Melbourne",
+        countrySubdivision: "Victoria",
+        countrySubdivisionName: "Victoria",
+        countrySubdivisionCode: "VIC",
+        postalCode: "3000",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress: "287-297 Queen Street, Melbourne CBD, Victoria, 3000",
+        localName: "Melbourne CBD",
+      },
+      position: {
+        lat: -37.811804,
+        lon: 144.958966,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -37.8109,
+          lon: 144.95783,
+        },
+        btmRightPoint: {
+          lat: -37.8127,
+          lon: 144.9601,
+        },
+      },
+      entryPoints: [
+        {
+          type: "main",
+          position: {
+            lat: -37.81175,
+            lon: 144.95914,
+          },
+        },
+      ],
+    },
+    {
+      type: "Address Range",
+      id: "qNAW4MF5aeTWw_vBm-9XIw",
+      score: 6.3046283722,
+      address: {
+        streetNumber: "297",
+        streetName: "Queen Street",
+        municipalitySubdivision: "Melbourne CBD",
+        municipality: "Melbourne",
+        countrySecondarySubdivision: "Melbourne",
+        countrySubdivision: "Victoria",
+        countrySubdivisionName: "Victoria",
+        countrySubdivisionCode: "VIC",
+        postalCode: "3000",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress: "297 Queen Street, Melbourne CBD, Victoria, 3000",
+        localName: "Melbourne CBD",
+      },
+      position: {
+        lat: -37.811595,
+        lon: 144.959014,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -37.81135,
+          lon: 144.95897,
+        },
+        btmRightPoint: {
+          lat: -37.81158,
+          lon: 144.95907,
+        },
+      },
+      addressRanges: {
+        rangeLeft: "297 - 299",
+        from: {
+          lat: -37.81158,
+          lon: 144.95907,
+        },
+        to: {
+          lat: -37.81135,
+          lon: 144.95897,
+        },
+      },
+    },
+    {
+      type: "Address Range",
+      id: "1qmQpdI_mdwAGWlCvfW5SA",
+      score: 6.3046283722,
+      address: {
+        streetNumber: "297",
+        streetName: "Queen Street",
+        municipalitySubdivision: "Elliminyt",
+        municipality: "Colac",
+        countrySecondarySubdivision: "West Coast Victoria",
+        countrySubdivision: "Victoria",
+        countrySubdivisionName: "Victoria",
+        countrySubdivisionCode: "VIC",
+        postalCode: "3250",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress: "297 Queen Street, Elliminyt, Victoria, 3250",
+        localName: "Elliminyt",
+      },
+      position: {
+        lat: -38.361106,
+        lon: 143.588872,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -38.35905,
+          lon: 143.58872,
+        },
+        btmRightPoint: {
+          lat: -38.36225,
+          lon: 143.58931,
+        },
+      },
+      addressRanges: {
+        rangeLeft: "319 - 257",
+        from: {
+          lat: -38.36225,
+          lon: 143.58872,
+        },
+        to: {
+          lat: -38.35905,
+          lon: 143.58931,
+        },
+      },
+    },
+    {
+      type: "Address Range",
+      id: "QZJNQM577t4iVdi6tXC-tg",
+      score: 6.3046283722,
+      address: {
+        streetNumber: "297",
+        streetName: "Queen Street",
+        municipalitySubdivision: "Brisbane CBD",
+        municipality: "Brisbane",
+        countrySecondarySubdivision: "Brisbane",
+        countrySubdivision: "Queensland",
+        countrySubdivisionName: "Queensland",
+        countrySubdivisionCode: "QLD",
+        postalCode: "4000",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress: "297 Queen Street, Brisbane CBD, Queensland, 4000",
+        localName: "Brisbane CBD",
+      },
+      position: {
+        lat: -27.467177,
+        lon: 153.028513,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -27.46705,
+          lon: 153.02836,
+        },
+        btmRightPoint: {
+          lat: -27.46723,
+          lon: 153.0286,
+        },
+      },
+      addressRanges: {
+        rangeLeft: "305 - 289",
+        from: {
+          lat: -27.46705,
+          lon: 153.0286,
+        },
+        to: {
+          lat: -27.46723,
+          lon: 153.02836,
+        },
+      },
+    },
+    {
+      type: "Address Range",
+      id: "J0cYmQPeGsMFuC9w5u5Wog",
+      score: 6.3046283722,
+      address: {
+        streetNumber: "297",
+        streetName: "Queen Street",
+        municipality: "Roebourne",
+        countrySecondarySubdivision: "De Gray",
+        countrySubdivision: "Western Australia",
+        countrySubdivisionName: "Western Australia",
+        countrySubdivisionCode: "WA",
+        postalCode: "6718",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress: "297 Queen Street, Roebourne, Western Australia, 6718",
+        localName: "Roebourne",
+      },
+      position: {
+        lat: -20.777384,
+        lon: 117.144019,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -20.77732,
+          lon: 117.14353,
+        },
+        btmRightPoint: {
+          lat: -20.77735,
+          lon: 117.14485,
+        },
+      },
+      addressRanges: {
+        rangeRight: "9 - 785",
+        from: {
+          lat: -20.77735,
+          lon: 117.14353,
+        },
+        to: {
+          lat: -20.77732,
+          lon: 117.14485,
+        },
+      },
+    },
+    {
+      type: "Address Range",
+      id: "h61i9CwzMoqjMG-s1KdMHg",
+      score: 6.3046283722,
+      address: {
+        streetNumber: "297",
+        streetName: "Queen Street",
+        municipality: "Grafton",
+        countrySecondarySubdivision: "Mid North Coast",
+        countrySubdivision: "New South Wales",
+        countrySubdivisionName: "New South Wales",
+        countrySubdivisionCode: "NSW",
+        postalCode: "2460",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress: "297 Queen Street, Grafton, New South Wales, 2460",
+        localName: "Grafton",
+      },
+      position: {
+        lat: -29.668374,
+        lon: 152.944642,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -29.66838,
+          lon: 152.94456,
+        },
+        btmRightPoint: {
+          lat: -29.66907,
+          lon: 152.94469,
+        },
+      },
+      addressRanges: {
+        rangeLeft: "299 - 297",
+        from: {
+          lat: -29.66907,
+          lon: 152.94456,
+        },
+        to: {
+          lat: -29.66838,
+          lon: 152.94469,
+        },
+      },
+    },
+    {
+      type: "Address Range",
+      id: "XNXHN_TkNk_QSjPR8ryHtQ",
+      score: 6.3046283722,
+      address: {
+        streetNumber: "297",
+        streetName: "Queen Street",
+        municipalitySubdivision: "Altona",
+        municipality: "Melbourne",
+        countrySecondarySubdivision: "Melbourne",
+        countrySubdivision: "Victoria",
+        countrySubdivisionName: "Victoria",
+        countrySubdivisionCode: "VIC",
+        postalCode: "3018",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress: "297 Queen Street, Altona, Victoria, 3018",
+        localName: "Altona",
+      },
+      position: {
+        lat: -37.873873,
+        lon: 144.78839,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -37.87384,
+          lon: 144.78807,
+        },
+        btmRightPoint: {
+          lat: -37.87392,
+          lon: 144.78837,
+        },
+      },
+      addressRanges: {
+        rangeRight: "297 - 297",
+        from: {
+          lat: -37.87392,
+          lon: 144.78837,
+        },
+        to: {
+          lat: -37.87384,
+          lon: 144.78807,
+        },
+      },
+    },
+    {
+      type: "POI",
+      id: "JZGwML3-eVbWLjVe4BwbdA",
+      score: 4.6736936569,
+      info: "search:ta:036007000221418-AU",
+      poi: {
+        name: "Queen Street",
+        categorySet: [
+          {
+            id: 7376,
+          },
+        ],
+        categories: ["important tourist attraction"],
+        classifications: [
+          {
+            code: "IMPORTANT_TOURIST_ATTRACTION",
+            names: [
+              {
+                nameLocale: "en-US",
+                name: "important tourist attraction",
+              },
+            ],
+          },
+        ],
+      },
+      address: {
+        streetName: "Queen Street",
+        municipalitySubdivision: "Woollahra",
+        municipality: "Sydney",
+        countrySecondarySubdivision: "Sydney",
+        countrySubdivision: "New South Wales",
+        countrySubdivisionName: "New South Wales",
+        countrySubdivisionCode: "NSW",
+        postalCode: "2025",
+        countryCode: "AU",
+        country: "Australia",
+        countryCodeISO3: "AUS",
+        freeformAddress: "Queen Street, Woollahra, New South Wales, 2025",
+        localName: "Woollahra",
+      },
+      position: {
+        lat: -33.888242,
+        lon: 151.23774,
+      },
+      viewport: {
+        topLeftPoint: {
+          lat: -33.88734,
+          lon: 151.23666,
+        },
+        btmRightPoint: {
+          lat: -33.88914,
+          lon: 151.23882,
+        },
+      },
+      entryPoints: [
+        {
+          type: "main",
+          position: {
+            lat: -33.88832,
+            lon: 151.23781,
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export const EMPTY_RESPONSE = {
+  summary: {
+    query: "dwcwvwvwv",
+    queryType: "NON_NEAR",
+    queryTime: 16,
+    numResults: 0,
+    offset: 0,
+    totalResults: 0,
+    fuzzyLevel: 2,
+    queryIntent: [],
+  },
+  results: [],
+};
+
+export const BREAKING_RESPONSE = {
+  invalid: true,
+};
